0aac8bd5f0303bcf16d0bd10022a8a060204e685
root <marianna.shvartsapel@gmail.com>
2018-09-20 18:04:08 +0000

    feat: Add tabs to the home page; add BriefQuestionsList component to make the code more DRY

---
+++ src/components/BriefQuestionsList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import BriefQuestion from './BriefQuestion';
+
+function BriefQuestionsList (props) {
+  	const { idsList, emptyListNote } = props;
+  
+	return (	
+      		<div>
+                {
+                    idsList.length
+                        ? idsList.map((id) => (
+                            <BriefQuestion key={id} id={id} />
+                        ))
+                        : <Typography component='p' align='center'>
+							{emptyListNote}
+                          </Typography>
+                }
+			</div>
+	);
+}
+
+export default BriefQuestionsList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { withStyles } from '@material-ui/core/styles';
 import BriefQuestionsList from './BriefQuestionsList';
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
 class Home extends Component {
-    
+    state = {
+    	value: 0,
+  	};
+
+	handleChange = (event, value) => {
+    	this.setState({ value });
+	};
+
     render () {
-        const { answeredQuestionIds, unansweredQuestionIds } = this.props;
+        const { answeredQuestionIds, unansweredQuestionIds, classes } = this.props;
+    	const { value } = this.state;
 
-        return (
-            <div>
-				<BriefQuestionsList 
-          			title='Answered Questions'
-          			idsList={answeredQuestionIds}
-          			emptyListNote='No Answered Questions yet! What are you waiting for???'
-				/>
-
-				<BriefQuestionsList 
-          			title='Unanswered Questions'
-          			idsList={unansweredQuestionIds}
-          			emptyListNote='No more Unswered Questions! Time to create some new ones! '
-				/>
-            </div>
-        );
-    }
+return (
+      <div className={classes.root}>
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Unanswered Questions" />
+            <Tab label="Answered Questions" />
+          </Tabs>
+        {value === 0 && <BriefQuestionsList 
+        	idsList={unansweredQuestionIds}
+        	emptyListNote='No more Unswered Questions! Time to create some new ones! '
+		/>}
+        {value === 1 && <BriefQuestionsList 
+        	idsList={answeredQuestionIds}
+        	emptyListNote='No Answered Questions yet! What are you waiting for???'
+		/>}
+      </div>
+    );
+	}
 }
 
 function mapStateToProps ({ authedUser, questions, users }) {
@@ -41,4 +60,5 @@ function mapStateToProps ({ authedUser, questions, users }) {
     }
 }
 
-export default connect(mapStateToProps)(Home);
+//export default connect(mapStateToProps)(withStyles(styles)(App));
+export default connect(mapStateToProps)(withStyles(styles)(Home));
