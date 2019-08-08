f4f624abd2e2d86524b168fe70c4b883f44a2d5f
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2019-08-08 09:51:19 -0400

    style: Improve formatting using Prettier

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -5,64 +5,68 @@ export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const ADD_QUESTION = 'ADD_QUESTION';
 export const ADD_ANSWER = 'ADD_ANSWER';
 
-export function receiveQuestions (questions) {
-    return {
-        type: RECEIVE_QUESTIONS,
-        questions
-    }
+export function receiveQuestions(questions) {
+	return {
+		type: RECEIVE_QUESTIONS,
+		questions
+	};
 }
 
-function addQuestion (question) {
-    return {
-        type: ADD_QUESTION,
-        question
-    }
+function addQuestion(question) {
+	return {
+		type: ADD_QUESTION,
+		question
+	};
 }
 
-function addAnswer ({ qid, answer, authedUser }) {
-    return {
-        type: ADD_ANSWER,
-        answerInfo: {
-          	qid,
-            answer,
-            authedUser
-        }
-    }
+function addAnswer({ qid, answer, authedUser }) {
+	return {
+		type: ADD_ANSWER,
+		answerInfo: {
+			qid,
+			answer,
+			authedUser
+		}
+	};
 }
 
 //async action creators
-export function handleAddQuestion (optionOne, optionTwo) {
-    return (dispatch, getState) => {
-        const { authedUser } = getState();
+export function handleAddQuestion(optionOne, optionTwo) {
+	return (dispatch, getState) => {
+		const { authedUser } = getState();
 
-        dispatch(showLoading());
+		dispatch(showLoading());
 
-        return saveQuestion({
-            optionOneText: optionOne,
-            optionTwoText: optionTwo,
-            author: authedUser
-        })
-        .then((question) => dispatch(addQuestion(question)))
-        .then(() => dispatch(hideLoading()));
-    }
+		return saveQuestion({
+			optionOneText: optionOne,
+			optionTwoText: optionTwo,
+			author: authedUser
+		})
+			.then((question) => dispatch(addQuestion(question)))
+			.then(() => dispatch(hideLoading()));
+	};
 }
 
-export function handleAddAnswer (qid, answer) {
-    return (dispatch, getState) => {
-        const { authedUser } = getState();
+export function handleAddAnswer(qid, answer) {
+	return (dispatch, getState) => {
+		const { authedUser } = getState();
 
-        dispatch(showLoading());
-      
-        return saveQuestionAnswer({
-          	qid,
-            answer,
-            authedUser
-        }) 
-        .then(() => dispatch(addAnswer({
-          	qid,
-            answer,
-            authedUser
-        })))
-        .then(() => dispatch(hideLoading()));
-    }
+		dispatch(showLoading());
+
+		return saveQuestionAnswer({
+			qid,
+			answer,
+			authedUser
+		})
+			.then(() =>
+				dispatch(
+					addAnswer({
+						qid,
+						answer,
+						authedUser
+					})
+				)
+			)
+			.then(() => dispatch(hideLoading()));
+	};
 }
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,14 +4,13 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveQuestions } from './questions';
 
-export function handleInitialData () {
-    return (dispatch) => {
-        dispatch(showLoading());
-        return getInitialData()
-            .then(({ users, questions }) => {
-                dispatch(receiveUsers(users));
-                dispatch(receiveQuestions(questions));
-                dispatch(hideLoading());
-            })
-    }
+export function handleInitialData() {
+	return (dispatch) => {
+		dispatch(showLoading());
+		return getInitialData().then(({ users, questions }) => {
+			dispatch(receiveUsers(users));
+			dispatch(receiveQuestions(questions));
+			dispatch(hideLoading());
+		});
+	};
 }
