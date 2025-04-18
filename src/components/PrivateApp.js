132c250cb335490538e1ec52de1fed3b0bbc304d
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2025-04-18 15:51:25 -0400

    fix: Replace BrowserRouter with HashRouter for GitHub Pages support

--- src/components/PrivateApp.js
+++ src/components/PrivateApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import NavigationBar from './NavigationBar';
 import Home from './Home';
